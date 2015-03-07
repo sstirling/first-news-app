4f2847c39222cc6e4f8ac9b8242719d7bcdb1fec
sstirling2 <sstirling@njadvancemedia.com>
2015-03-07 11:05:28 -0500

    Added map

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import csv
 from flask import Flask
 from flask import render_template
+from flask import abort
 app = Flask(__name__)
 
 def get_csv():
@@ -24,7 +25,7 @@ def detail(row_id):
 	for obj in object_list:
 		if obj['id'] == row_id:
 			return render_template(template, object=obj)
-	
+	abort(404)
 
 if __name__ == "__main__":
 	app.run(debug=True, use_reloader=True)
